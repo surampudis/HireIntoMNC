813a377afbc3fb03febfa53f8a4ec6ba46a70c75
surampudis <shanthi.surampudi@gmail.com>
2021-08-09 06:05:32 -0500

    email changes

--- js/src.js
+++ js/src.js
@@ -11,3 +11,24 @@ function validate(){
     }
 
 }
+
+
+function sendEmail() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var subject = document.getElementById("subject").value;
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "hireintomnc@gmail.com",
+	Password : "hireinto@123",
+	To : 'hrconnect@himsoftware.com',
+	From : "hireintomnc@gmail.com",
+	Subject : fname  +" "+ lname + " sent a new message from website",
+	Body : subject,
+	}).then(
+		message => alert("mail sent successfully. Thak you "+ fname  +" for your interest. Click on cancel to return to home page")
+      
+	);  
+
+ 
+}
